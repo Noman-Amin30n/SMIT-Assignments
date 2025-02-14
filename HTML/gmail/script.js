4408695d5ea7792740353062a8d6e0dc421cbd35
Muhammad Noman Ameen <nomankhan30n@gmail.com>
2025-02-14 15:54:53 +0500

    Gmail clone completed

--- HTML/gmail/script.js
+++ HTML/gmail/script.js
@@ -6,16 +6,34 @@ const hamburger = document.getElementById("Hamburger");
 const mainLeft = document.querySelector(".main-left");
 const mainRight = document.querySelector(".main-right")
 
+
+if (window.innerWidth < 768) {
+    mainLeft.classList.add("Toggle-main-left");
+    mainRight.style.marginLeft = "16px";
+}
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 768) {
+        mainLeft.classList.add("Toggle-main-left");
+        mainRight.style.marginLeft = "16px";
+    } else {
+        mainLeft.classList.remove("Toggle-main-left");
+        mainRight.style.marginLeft = "0px";
+    }
+});
+
 hamburger.addEventListener("click", () => {
     mainLeft.classList.toggle("Toggle-main-left");
     if (mainLeft.classList.contains("Toggle-main-left")) {
         mainRight.style.marginLeft = "16px";
     } else {
-        mainRight.style.marginLeft = "0px";
+        if(window.innerWidth > 768){
+            mainRight.style.marginLeft = "0px";
+        }
     }
 });
 
-angle.style.transition = "0.2s";
+angle.style.transition = "all 0.2s ease-in-out";
 text.style.display = "inline-block";
 
 more.addEventListener("click", () => {
@@ -26,4 +44,21 @@ more.addEventListener("click", () => {
         text.textContent = "More";
     }
 });
+
+const descriptions = document.querySelectorAll(".Description");
+
+function isTextOverflowing(element) {
+    return element.offsetWidth < element.scrollWidth;
+}
+
+function handleTextOverflow(element) {
+    if (isTextOverflowing(element)) {
+        element.classList.add("Text-exceed")
+    }
+}
+
+descriptions.forEach(description => {
+    handleTextOverflow(description);
+});
+
 
